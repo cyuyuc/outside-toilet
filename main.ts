57cf8a8aac0324660022c2304a47d65461846e1d
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-24 23:22:38 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,25 @@
+radio.onReceivedValue(function (name, value) {
+    available = value
+})
+let available = 0
+basic.clearScreen()
+radio.setGroup(11)
+radio.setTransmitPower(7)
+basic.forever(function () {
+    if (available == 1) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(500)
+        basic.showIcon(IconNames.Happy)
+    } else if (available == 2) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (available == 3) {
+        basic.showIcon(IconNames.No)
+        basic.pause(250)
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showIcon(IconNames.No)
+        music.playMelody("C5 A B G A F G E ", 120)
+        basic.showString("error")
+    }
+    available = 0
+})
