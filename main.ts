7ba3407f15931ce4dc0a4d6b1462556d57ef0997
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-27 21:50:56 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,24 +2,22 @@ radio.onReceivedValue(function (name, value) {
     availability = value
 })
 let availability = 0
+music.stopAllSounds()
 basic.clearScreen()
 radio.setGroup(11)
 radio.setTransmitPower(7)
+music.setVolume(255)
 basic.forever(function () {
     if (availability == 1) {
         basic.showIcon(IconNames.Yes)
-        basic.pause(500)
-        basic.showIcon(IconNames.Happy)
     } else if (availability == 2) {
         basic.showIcon(IconNames.No)
         basic.pause(10)
         basic.showIcon(IconNames.Asleep)
     } else if (availability == 3) {
         basic.showIcon(IconNames.No)
-        basic.pause(250)
-        basic.showIcon(IconNames.Sad)
     } else {
-        basic.showIcon(IconNames.No)
+        basic.showIcon(IconNames.Sad)
         music.playMelody("C5 A B G A F G E ", 120)
         basic.showString("error")
     }
